e869f4ae5f51a9f9e910ee5bfaae6210a75b4c07
Will Sanders <will@clickatwill.com>
2024-07-25 22:21:46 -0600

    Add comments to why dates return the values they do

--- DataTypes/dates.js
+++ DataTypes/dates.js
@@ -1,13 +1,17 @@
 // Creating dates
-let date1 = new Date("2023-01-01");
-console.log(date1);
-let date2 = new Date("January 1, 2023");
-console.log(date2);
+let date1 = new Date("2023-01-01"); //Code assumes you are entering in UTC time
+console.log(date1); //But writes out your current time based on computer clock
+
+let date2 = new Date("January 1, 2023"); 
+console.log(date2); //Assumes your current time
+
 let date3 = new Date(); // now
-console.log(date3);
+console.log(date3); //Your current time
+
 let date4 = new Date("2023-01-01T02:30:04.312-05:00");
 console.log(date4);
-let date5 = new Date(2023, 0, 1, 2, 30, 4);
+
+let date5 = new Date(2023, 0, 1, 2, 30, 4); //Specify exact date and time
 console.log(date5);
 
 // Accessing elements from a date
